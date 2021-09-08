1b19ca6d34919a3152e715c23a718379b5594ca1
Naran Torguud <ntorguud@gmail.com>
2021-09-07 20:24:52 -0700

    Add active red dot switch

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Toglogchiin eeljiig hadgalah var. Player1 = 0, player2 = 1;
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Toglogchiin tsugluulsan onoog hadgalah var
 var scores = [0, 0];
@@ -7,18 +7,12 @@ var scores = [0, 0];
 //Toglogchiin eeljindee tsugluulj bga onoog hadgalah var
 var roundScore = 0;
 
-
 /* <div class="player-score" id="score-0">43</div> */
 // window.document.querySelector('#score-0').textContent = diceNumber; //window global uchir zaawal bichih shaardlagagaui
-
-// document.querySelector('#score-1').innerHTML = dice;
-
-
 
 //Program ehlehed beltgey
 /* <img src="images/dice-5.png" alt="Dice" class="dice"></img> */
 var diceDom = document.querySelector('.dice');
-
 diceDom.style.display ='none';
 
 document.getElementById('score-0').textContent = '0'; document.getElementById('score-1').textContent = '0';
@@ -27,10 +21,7 @@ document.getElementById('current-1').textContent = '0';
 
 //Roll Dice tovchoo ajilladag bolgoh event-listener
 /* <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button> */
-document.querySelector('.btn-roll').addEventListener('click', rollDice //It can be anonymous function, cuz it's used only once
-);
-
-function rollDice() { //this is a callback function
+document.querySelector('.btn-roll').addEventListener('click', function() { //this is a callback function
     //Shoonii ali talaaraa buusniig hadgalah var, random 1-6 values should be kept in this var
     var diceNumber = Math.ceil(Math.random() * 6); //Why 0.88977 garch irj bna ve?
 
@@ -40,13 +31,32 @@ function rollDice() { //this is a callback function
     //Random number-n dice-iin zurgiig gargaj irne.
     diceDom.src = 'images/dice-' + diceNumber + '.png';
 
-    //Shidsen toog active playeriin onoon deer nemeh event-listener, except 1. Number one makes score 0.
+    //Shidsen toog active playeriin onoon deer nemeh event-listener, except 1. 
     if(diceNumber !== 1) {
         //Add that number as a score to an active player.
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-        //Change players' turn
+        //1 gedeg number can change players' turn and make score 0. in the screen.
 
+        //We still need to make score 0 in the saved container, which is roundScore
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0; //activePlayer soligdohoos umnu 1 onoo avsan player-iin onoo 0 boloh estoi tul condition shalgahaas umnu bichih estoi.
+
+        if(activePlayer === 0) {
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
+        }
     }
-}
+} //It can be anonymous function, cuz it's used only once
+);
+//===deerh if/else code-toi adilhan: activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+//Red dot-iig shiljuuleh code bichiy
+document.querySelector('.player-0-panel').classList.toggle('active');
+
+document.querySelector('.player-1-panel').classList.toggle('active');
+
+//Player hoorond shiljih ued dice-iig tur alga bolgoy
+diceDom.style.display ='none';
