ee27fc60194ff13e30c4f24456bec03dc4431307
Naran Torguud <ntorguud@gmail.com>
2021-09-08 14:54:07 -0700

    ..

--- js/app.js
+++ js/app.js
@@ -25,7 +25,8 @@ function initGame() { //cmd + function ==== function ruu userne.
     /* <img src="images/dice-5.png" alt="Dice" class="dice"></img> */
     diceDom.style.display ='none';
 
-    document.getElementById('score-0').textContent = '0'; document.getElementById('score-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0'; 
+    document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
     //Players' names-iig togloom duussanii daraa butsaaj delgetsen deer gargah
